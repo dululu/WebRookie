8b3a2e6511be15ecfd721f4f60e92e8ea366f0e9
xhqaq <2399029980@qq.com>
2022-10-20 22:54:38 +0800

    chill

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,11 @@
-let a = [1, 2, "a", "yxc"];
+let div = document.querySelector('div')
 
-let b = [
-    1,  // 变量
-    "yxc",  // 变量
-    ['a', 'b', 3],  // 数组
-    function () {  // 函数
-        console.log("Hello World");
-    },
-    { name: "yxc", age: 18 }  // 对象
-];
 
 let main = function() {
-    console.log(b[2][1])
-    console.log(b[1])
-    console.log(b[3]())
-
+    div.addEventListener('contextmenu',function(event)
+    {
+        console.log(event.type);
+    });
 }
 
 export {
