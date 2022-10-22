268db41e649df4ff2675e03bc65378c67e282438
xhqaq <2399029980@qq.com>
2022-10-22 23:19:22 +0800

    10/22

---
+++ static/js/3.js
@@ -0,0 +1,11 @@
+let main = function() {
+ 
+    $('div').css('cursor','pointer')
+    $('div').click(function (){
+        window.open("https://www.acwing.com")
+
+    })
+}
+export {
+    main
+}
---
+++ static/js/time.js
@@ -0,0 +1,9 @@
+let a = function() {
+    let starttime = new Date();
+    console.log(starttime.getDay());
+    console.log(starttime.getFullYear());
+    console.log(starttime.getMonth());
+}
+export {
+    a
+}
